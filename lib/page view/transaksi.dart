9279af74cd9045d09e3fad12c1be09b574cc262e
Malikibrah1m <malikcitizen@gmail.com>
2022-11-25 11:06:43 +0700

    tambah content tabbar

--- lib/page view/transaksi.dart
+++ lib/page view/transaksi.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projectcarwash/page%20view/startscreen.dart';
 
 class Transaksi extends StatefulWidget {
   const Transaksi({super.key});
@@ -11,18 +12,72 @@ class _TransaksiState extends State<Transaksi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Column(
-            children: [
-              Padding(padding: EdgeInsets.all(20)),
-              Text(
-                "Transaki",
-              )
-            ],
+        body: ListView(
+      children: [
+        Card(
+          child: ListTile(
+            isThreeLine: true,
+            leading: Icon(Icons.dataset, size: 56),
+            title: Text(
+              "Nama orang yang reservasi",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: "Inter",
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Plat nomor ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: "Inter",
+                    fontSize: 18,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 2.0),
+                  child: Text(
+                    "Tipe pencucian ",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: "Inter",
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 2.0),
+                  child: Text(
+                    "Total harga",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: "Inter",
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(shape: StadiumBorder()),
+                      onPressed: () {},
+                      label: Text("Verifikasi"),
+                      icon: Icon(
+                        Icons.check_circle,
+                        color: Color(0xff00ff28),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+        )
+      ],
+    ));
   }
 }
